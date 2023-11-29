9d8a4569e5c20baa8bf8bb85216e006e8c4e05c7
Federico Perez <damipe90@gmail.com>
2023-11-28 22:55:08 -0300

    first commit

---
+++ src/routes/carts.router.js
@@ -0,0 +1,20 @@
+const { Router } = require('express')
+const CartsManagerFile = require('../managers/cartsManager')
+const cartsService = new CartsManagerFile()
+const router = Router()
+
+router.get('/:cid', async (req,res)=>{
+    try {
+        const {cid} = req.params
+        const cart = await cartsService.getCartById(parseInt(cid))
+        res.send({
+            status: 'success',
+            payload: cart
+        })
+        
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+module.exports = router
